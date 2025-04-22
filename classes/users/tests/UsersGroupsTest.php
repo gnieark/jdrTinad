cde0936bef37bb788a038de275ec23744a80f2b8
Gnieark <gnieark@tinad.fr>
2025-04-22 15:53:44 +0200

    test pass 2 ?

--- classes/users/tests/UsersGroupsTest.php
+++ classes/users/tests/UsersGroupsTest.php
@@ -9,13 +9,15 @@ register_shutdown_function(function () {
 class UsersGroupsTest extends TestCase {
 
     protected PDO $pdo;
+    protected $first = true;
 
     protected function setUp(): void {
-        if (!isset($this->pdo)){
+        if ($this->first){
             //$this->pdo = new PDO('sqlite::memory:');
             $this->pdo= new PDO('sqlite:test.db');
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             UserGroupManager::createTables($this->pdo);
+            $this->first = false;
         }
     }
 
