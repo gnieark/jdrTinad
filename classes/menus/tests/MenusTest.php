0b4a38ecdedbd84d3aea3269fdffe7e15e954070
Gnieark <gnieark@tinad.fr>
2025-04-23 11:14:30 +0200

    prepare menus class for unit tests

--- classes/menus/tests/MenusTest.php
+++ classes/menus/tests/MenusTest.php
@@ -2,16 +2,23 @@
 
 use PHPUnit\Framework\TestCase;
 
-class MenuTest extends TestCase {
+class MenusTest extends TestCase {
 
     protected PDO $pdo;
 
 
     protected function setUp(): void {
-
+        require_once(   dirname(__FILE__) .  "/TestMenusClass.php" );
     }
 
-    public function testCreateTables():void{
+    public function testLoadMenusDefinitions():void{
+        $_SERVER["REQUEST_URI"] = "/kdsjhrgtkrgy";
+        $mManager = new Menus_manager();
+        $mManager->add_menus_items_from_json_file( dirname(__FILE__) . "/menustests.json");
+        $currentMenu = $mManager->get_current_menu();
+        //should be 404 menu
+        $this->assertEquals("404", $currentMenu->get_name() );
+   
 
     }
 
