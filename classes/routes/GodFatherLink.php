a8d9c4a5cabf89879e71ae3a3c2ebfb406ba9f11
Gnieark <gnieark@tinad.fr>
2025-05-11 21:51:53 +0200

    wip

--- classes/routes/GodFatherLink.php
+++ classes/routes/GodFatherLink.php
@@ -1,8 +1,11 @@
 <?php
 class GodFatherLink extends Route{
     static public function get_content_html(User $user):string{
-        return "hey!";
+        $tpl = new TplBlock();
+        return $tpl->applyTplFile("../templates/godfather.html");
     }
-
+    static public function get_custom_css(User $user):string{
+        return file_get_contents ("../templates/godfather.css");
+    }
 
 }
