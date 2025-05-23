281f00963a25b627da5500155d225f8f2a09df87
Gnieark <gnieark@tinad.fr>
2025-05-23 13:18:03 +0200

    esthetique

--- classes/routes/MainPage.php
+++ classes/routes/MainPage.php
@@ -1,12 +1,7 @@
 <?php
 class MainPage extends Route{
     static public function get_content_html(User $user):string{
-        return '
-<div class="logo-container">
-  <img src="/logo.png" alt="Logo jdr.tinad.fr" class="site-logo">
-</div>
-<div class="tutoslinks" ><ul><li><a href="/tutos/mecaniques">Mécaniques d\'un JDR (dés, compétences...)</a></li>
-<li><a href="/tutos/aventurier">Tutoriel de l\'aventurier</a></li><li><a href="/tutos/mj">Tutoriel du maître du jeu</a></li></ul></div>';
+      return file_get_contents ("../templates/MainPage.html");
     }
 
     static public function get_custom_css(User $user):string{
